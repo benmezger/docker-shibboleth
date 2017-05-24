87b38f39045ef3a5965aff8f0d4723943a996c78
Skalldihor <skalldihor@riseup.net>
2017-05-24 02:56:35 -0300

    Dockerfile e entrypoint para o IdP Docker, agora compilando o projeto Shibboleth

--- idp/docker-entrypoint.sh
+++ idp/docker-entrypoint.sh
@@ -8,4 +8,11 @@ export PATH=$PATH:$JAVA_HOME/bin
 
 sed -i "s/^-Xmx.*$/-Xmx$JETTY_MAX_HEAP/g" /opt/iam-jetty-base/start.ini
 
+/opt/shibboleth-idp/bin/build.sh -Didp.target.dir=/opt/shibboleth-idp
+
+chown -R jetty:root /opt/shibboleth-idp
+chmod 755 -R /opt/shibboleth-idp/
+
+#chmod 755 /opt/shibboleth-idp/conf/attribute-resolver.xml
+
 /etc/init.d/jetty run 
