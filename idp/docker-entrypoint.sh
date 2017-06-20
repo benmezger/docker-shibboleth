4029a7ffde1806f517c5f5567fa94bfc1c436da5
Jucélio Jair Silva <Juclio Jair Silva>
2017-06-20 16:26:14 -0300

    Nova configuração do java 8u131, portas do containner

--- idp/docker-entrypoint.sh
+++ idp/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -x
 
-export JAVA_HOME=/opt/jre1.8.0_60
+export JAVA_HOME=/opt/jre1.8.0_131
 export JETTY_HOME=/opt/jetty/
 export JETTY_BASE=/opt/iam-jetty-base/
 export PATH=$PATH:$JAVA_HOME/bin
